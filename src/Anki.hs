6c028064f5b6110d592f752fc09c09f6e8125f6d
vivaswt <vivaswt@gmail.com>
2025-03-11 11:56:46 +0900

    Add support for media files in flashcards and enhance JSON parsing

--- src/Anki.hs
+++ src/Anki.hs
@@ -10,12 +10,13 @@ where
 
 import Control.Exception.Safe (SomeException (SomeException), catch)
 import Control.Monad.Trans.Except (ExceptT (ExceptT), throwE)
-import Data.Aeson (FromJSON, Value, eitherDecode, withObject, (.:), (.:?))
+import Data.Aeson (FromJSON, Value (..), eitherDecode, withObject, (.:), (.:?))
 import Data.Aeson.QQ (aesonQQ)
 import Data.Aeson.Types (FromJSON (parseJSON), Parser)
 import Data.Bifunctor (Bifunctor (first))
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
+import qualified Data.Vector as V
 import Network.HTTP.Simple
   ( getResponseBody,
     httpLBS,
@@ -34,12 +35,29 @@ data AnkiAddNoteResult
 instance FromJSON AnkiAddNoteResult where
   parseJSON = parseAnkiAddNoteResult
 
+data AnkiStoreMediaFileResult
+  = AnkiStoreMediaFileResultSuccess MediaFileName
+  | AnkiStoreMediaFileResultFailure T.Text
+  deriving (Show)
+
+instance FromJSON AnkiStoreMediaFileResult where
+  parseJSON = parseAnkiStoreMediaFileResult
+
+type MediaFileName = T.Text
+
 parseAnkiAddNoteResult :: Value -> Parser AnkiAddNoteResult
 parseAnkiAddNoteResult = withObject "AnkiAddNoteResult" $ \o -> do
   e <- o .:? "error" :: Parser (Maybe T.Text)
   case e of
     Nothing -> AnkiAddNoteResultSuccess <$> o .: "result"
     Just err -> return $ AnkiAddNoteResultFailure err
+
+parseAnkiStoreMediaFileResult :: Value -> Parser AnkiStoreMediaFileResult
+parseAnkiStoreMediaFileResult = withObject "AnkiStoreMediaFileResult" $ \o -> do
+  e <- o .:? "error" :: Parser (Maybe T.Text)
+  case e of
+    Nothing -> AnkiStoreMediaFileResultSuccess <$> o .: "result"
+    Just err -> return $ AnkiStoreMediaFileResultFailure err
 
 addNoteOfFlashCards :: [NFC.FlashCard] -> ExceptT T.Text IO [AnkiAddNoteResult]
 addNoteOfFlashCards = mapM addNoteOfFlashCard
@@ -81,6 +99,8 @@ addNoteRequestBody flashCard =
                 "OtherIPA": #{NFC.flashCardOtherIPA flashCard'},
                 "MnemonicText": #{NFC.flashCardMnemonic flashCard'}
             },
+            "picture" : #{picutreRequestBody flashCard},
+            "audio" : #{audioRequestBody flashCard},
             "options": {
                 "allowDuplicate": false
             }
@@ -89,6 +109,46 @@ addNoteRequestBody flashCard =
   }|]
   where
     flashCard' = arrangeFlashCard flashCard
+
+picutreRequestBody :: NFC.FlashCard -> Value
+picutreRequestBody flashCard =
+  Array vs
+  where
+    vs = V.fromList $ map (\(NFC.Image u) -> fileBody "picture" "WordImage" u) blocks
+
+    blocks = filter predicate $ NFC.flashCardBlockChildren flashCard
+
+    predicate b = case b of
+      NFC.Image _ -> True
+      _ -> False
+
+    fileBody :: T.Text -> T.Text -> T.Text -> Value
+    fileBody name field url =
+      [aesonQQ| {
+      url : #{url},
+      filename : #{name},
+      fields : [#{field}]
+    } |]
+
+audioRequestBody :: NFC.FlashCard -> Value
+audioRequestBody flashCard =
+  Array vs
+  where
+    vs = V.fromList $ map (\(NFC.Audio u) -> fileBody "audio" "PronounceSentence" u) blocks
+
+    blocks = filter predicate $ NFC.flashCardBlockChildren flashCard
+
+    predicate b = case b of
+      NFC.Audio _ -> True
+      _ -> False
+
+    fileBody :: T.Text -> T.Text -> T.Text -> Value
+    fileBody name field url =
+      [aesonQQ| {
+      url : #{url},
+      filename : #{name},
+      fields : [#{field}]
+    } |]
 
 arrangeFlashCard :: NFC.FlashCard -> NFC.FlashCard
 arrangeFlashCard card =
@@ -110,4 +170,8 @@ splitAtParentheses s =
       fromMaybe "" (T.stripPrefix "(" t >>= T.stripSuffix ")")
 
 editQuestion :: T.Text -> T.Text -> T.Text
-editQuestion answer = T.replace "(…)" ("<b>" <> answer <> "</b>")
+editQuestion answer =
+  T.replace "(...)" replacement
+    . T.replace "(…)" replacement
+  where
+    replacement = "<b>" <> answer <> "</b>"
--- src/NotionFlashCard.hs
+++ src/NotionFlashCard.hs
@@ -4,19 +4,23 @@
 module NotionFlashCard
   ( getFlashCardsFromNotion,
     FlashCard (..),
+    BlockTypeObject (..),
+    testShowFlashCardSimple,
   )
 where
 
+import Control.Applicative (Alternative ((<|>)))
 import Control.Exception.Safe (SomeException (SomeException), catch)
 import Control.Monad ((<=<))
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Except (ExceptT (ExceptT), throwE)
+import Control.Monad.Trans.Except (ExceptT (ExceptT), runExceptT, throwE)
 import Data.Aeson
 import Data.Aeson.QQ (aesonQQ)
 import Data.Aeson.Types
 import Data.Bifunctor (Bifunctor (first))
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as TXT
 import Data.Vector as V (Vector)
 import Network.HTTP.Simple
@@ -38,36 +42,51 @@ data FlashCard = FlashCard
     flashCardIPA :: TXT.Text,
     flashCardSense :: TXT.Text,
     flashCardOtherIPA :: TXT.Text,
-    flashCardMnemonic :: TXT.Text
+    flashCardMnemonic :: TXT.Text,
+    flashCardBlockChildren :: [BlockTypeObject]
   }
   deriving (Show)
 
 instance FromJSON FlashCard where
   parseJSON = parseFlashCard
 
+type URL = TXT.Text
+
+data BlockTypeObject = Image URL | Audio URL | Others
+  deriving (Eq)
+
+instance FromJSON BlockTypeObject where
+  parseJSON = parseContentBlock
+
+instance Show BlockTypeObject where
+  show (Image url) = "Image: " ++ TXT.unpack url
+  show (Audio url) = "Audio: " ++ TXT.unpack url
+  show Others = "Others"
+
 getFlashCardsFromNotion :: ExceptT TXT.Text IO [FlashCard]
 getFlashCardsFromNotion = do
   token <- getBearerToken
-  let request =
-        addRequestHeader "Authorization" token
-          . addRequestHeader "Notion-Version" "2022-06-28"
-          . setRequestHeader "Content-Type" ["application/json"]
-          . setRequestBodyJSON bodyCondition
-          . setRequestMethod "POST"
-          $ parseRequest_ "https://api.notion.com/v1/databases/3f62546fdc9e47d1920756a6dbc55cfa/query"
-  response <- catch (httpLBS request) $
+  response <- catch (httpLBS . request $ token) $
     \(SomeException e) -> throwE $ "Failed to get flash cards from Notion " <> TXT.pack (show e)
 
   case getResponseStatusCode response of
     200 -> do
       let body = getResponseBody response
           cards = parseEither parseFlashCards =<< eitherDecode body
-      -- liftIO $ TIO.putStrLn . TL.toStrict . TLE.decodeUtf8 $ body
-      ExceptT . return . first TXT.pack $ cards
+      cards' <- ExceptT . return . first TXT.pack $ cards
+      mapM fillFlashCardBlockChildren cards'
     code ->
       throwE $
         "Failed to get flash cards from Notion : " <> (TXT.pack . show $ code)
   where
+    request t =
+      addRequestHeader "Authorization" t
+        . addRequestHeader "Notion-Version" "2022-06-28"
+        . setRequestHeader "Content-Type" ["application/json"]
+        . setRequestBodyJSON bodyCondition
+        . setRequestMethod "POST"
+        $ parseRequest_ "https://api.notion.com/v1/databases/3f62546fdc9e47d1920756a6dbc55cfa/query"
+
     bodyCondition =
       [aesonQQ|{
         "filter": {
@@ -99,7 +118,7 @@ parseFlashCard = withObject "FlashCards" $ \o -> do
   otherIPA <- getRichTextContent properties "その他発音"
   mnemonic <- getRichTextContent properties "語呂"
 
-  return $ FlashCard cardID question answer ipa sense otherIPA mnemonic
+  return $ FlashCard cardID question answer ipa sense otherIPA mnemonic []
   where
     getTitleContent :: Object -> Key -> Parser TXT.Text
     getTitleContent properties key = do
@@ -129,3 +148,89 @@ getBearerToken = do
   case token of
     Just t -> return . BC.pack $ "Bearer " ++ t
     Nothing -> throwE "PRIVATE_NOTION_TOKEN is not set"
+
+getPageContent :: TXT.Text -> ExceptT TXT.Text IO BL.ByteString
+getPageContent pageID = do
+  token <- getBearerToken
+  let request =
+        addRequestHeader "Authorization" token
+          . addRequestHeader "Notion-Version" "2022-06-28"
+          . setRequestHeader "Content-Type" ["application/json"]
+          . setRequestMethod "GET"
+          . parseRequest_
+          $ "https://api.notion.com/v1/blocks/" ++ TXT.unpack pageID ++ "/children"
+  response <- catch (httpLBS request) $
+    \(SomeException e) ->
+      throwE $ "Failed to get flash card content from Notion : " <> TXT.pack (show e)
+
+  case getResponseStatusCode response of
+    200 -> do
+      return . getResponseBody $ response
+    code ->
+      throwE $
+        "Failed to get flash card content from Notion : "
+          <> (TXT.pack . show $ code)
+          <> " "
+          <> (TXT.pack . show $ getResponseBody response)
+
+parseContentBlock :: Value -> Parser BlockTypeObject
+parseContentBlock = withObject "Block" $ \o -> do
+  blockType <- o .: "type" :: Parser TXT.Text
+  case blockType of
+    "image" -> do
+      im <- o .: "image"
+      fl <- im .: "file" <|> im .: "external"
+      url <- fl .: "url"
+      return $ Image url
+    "audio" -> do
+      au <- o .: "audio"
+      fl <- au .: "file" <|> au .: "external"
+      url <- fl .: "url"
+      return $ Audio url
+    _ -> return Others
+
+parseContentBlocks :: Value -> Parser [BlockTypeObject]
+parseContentBlocks = withObject "Blocks" $ \o -> do
+  results <- o .: "results" :: Parser [Value]
+  mapM parseContentBlock results
+
+fillFlashCardBlockChildren :: FlashCard -> ExceptT TXT.Text IO FlashCard
+fillFlashCardBlockChildren card = do
+  children <- getFlashCardBlockChildren $ flashCardID card
+  return $ card {flashCardBlockChildren = children}
+
+getFlashCardBlockChildren :: TXT.Text -> ExceptT TXT.Text IO [BlockTypeObject]
+getFlashCardBlockChildren pageID = do
+  responseBody <- getPageContent pageID
+  let r = do
+        contentJSON <- eitherDecode responseBody
+        parseEither parseContentBlocks contentJSON
+  eitherString2EitherTText r
+
+eitherString2EitherTText :: Either String a -> ExceptT TXT.Text IO a
+eitherString2EitherTText = ExceptT . return . first TXT.pack
+
+showFlashCardSimple :: FlashCard -> IO ()
+showFlashCardSimple card = do
+  putStrLn $ "ID: " ++ TXT.unpack (flashCardID card)
+  putStrLn $ "Question: " ++ TXT.unpack (flashCardQuestion card)
+  putStrLn $ "Answer: " ++ TXT.unpack (flashCardAnswer card)
+  putStrLn $ "IPA: " ++ TXT.unpack (flashCardIPA card)
+  putStrLn $ "Sense: " ++ TXT.unpack (flashCardSense card)
+  putStrLn $ "Other IPA: " ++ TXT.unpack (flashCardOtherIPA card)
+  putStrLn $ "Mnemonic: " ++ TXT.unpack (flashCardMnemonic card)
+  putStrLn "Block Children:"
+  mapM_ showFlashCardBlockChildrenSimple (flashCardBlockChildren card)
+  putStrLn ""
+
+showFlashCardBlockChildrenSimple :: BlockTypeObject -> IO ()
+showFlashCardBlockChildrenSimple (Image url) = putStrLn $ "Image: " ++ TXT.unpack url
+showFlashCardBlockChildrenSimple (Audio url) = putStrLn $ "Audio: " ++ TXT.unpack url
+showFlashCardBlockChildrenSimple Others = putStrLn "Others"
+
+testShowFlashCardSimple :: IO ()
+testShowFlashCardSimple = do
+  cards <- runExceptT getFlashCardsFromNotion
+  case cards of
+    Left err -> putStrLn . TXT.unpack $ err
+    Right cs -> mapM_ showFlashCardSimple cs
